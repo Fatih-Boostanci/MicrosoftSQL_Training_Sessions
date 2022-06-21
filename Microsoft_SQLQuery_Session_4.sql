214d0f207874bba2c29b6fa3350a05e1e3025bdb
victor-eagle <101409465+victor-eagle@users.noreply.github.com>
2022-06-21 23:38:08 +0300

    Add files via upload

--- Microsoft_SQLQuery_Session_4.sql
+++ Microsoft_SQLQuery_Session_4.sql
@@ -85,6 +85,56 @@ FROM product.product as A
 INNER JOIN product.category as B
 ON A.category_id = B.category_id;
 
+SELECT S.first_name, S.last_name, R.store_name
+FROM sale.staff as S
+INNER JOIN sale.store as R
+ON S.store_id = R.store_id;
+
+
+--LEFT JOIN
+
+SELECT P.product_id, P.product_name, O.order_id
+FROM product.product as P
+LEFT JOIN sale.order_item as O
+ON P.product_id = O.product_id
+WHERE O.product_id IS NULL;
+
+
+
+SELECT P.product_id, P.product_name, S.*
+FROM product.product as P
+LEFT JOIN product.stock as S
+ON P.product_id = S.product_id
+WHERE P.product_id > 310;
+
+
+--RIGHT JOIN
+
+SELECT P.product_id, P.product_name, S.*
+FROM product.stock as S
+RIGHT JOIN product.product as P
+ON P.product_id = S.product_id
+WHERE S.product_id > 310;
+
+--FULL OUTER JOIN
+
+--Write a query that returns stock and order info together for all products(Top 100)
+select	top 100 A.product_id, B.store_id, B.quantity, C.order_id, C.list_price
+from	product.product A
+FULL OUTER JOIN product.stock B
+ON		A.product_id = B.product_id
+FULL OUTER JOIN sale.order_item C
+ON		A.product_id = C.product_id
+order by B.store_id;
+
+
+--CROSS JOIN
+
+SELECT	B.store_id, A.product_id, 0 quantity
+FROM	product.product A
+CROSS JOIN sale.store B
+WHERE	A.product_id NOT IN (SELECT product_id FROM product.stock)
+ORDER BY A.product_id, B.store_id;
 
 
 
